102c02f52da2d88731ccedf23c002552737f644a
likeme <33179335+likeme@users.noreply.github.com>
2020-10-01 16:59:10 +0530

    Create substring.py

---
+++ substring.py
@@ -0,0 +1,14 @@
+word = raw_input()
+  
+# returns first occurrence of Substring 
+result = word.find('geeks') 
+print ("Substring 'geeks' found at index:", result ) 
+  
+result = word.find('for') 
+print ("Substring 'for ' found at index:", result ) 
+  
+# How to use find() 
+if (word.find('pawan') != -1): 
+    print ("Contains given substring ") 
+else: 
+    print ("Doesn't contains given substring") 
